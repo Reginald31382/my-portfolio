e6bf723b396cf9abe94378b0f33b008105e18224
Reginald J Dixon <vassarjonez@yahoo.com>
2023-11-16 15:58:05 -0500

    edit image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Model from "./Model";
-import me2023 from "../images/me2023.jpg";
+import me from "../images/me2023.jpg";
 
 function About() {
   return (
     <>
       <Model
         title={"About"}
-        img={me2023}
+        img={me}
         paragraph1={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et magnis dis parturient montes nascetur. Maecenas pharetra convallis posuere morbi leo. Non nisi est sit amet facilisis magna etiam. Et egestas quis ipsum suspendisse ultrices. Mauris rhoncus aenean vel elit scelerisque. Duis at tellus at urna condimentum. Felis eget nunc lobortis mattis aliquam. Eget felis eget nunc lobortis mattis aliquam faucibus. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula."
         }
